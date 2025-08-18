e64b794731eb99d45991f778a726cc8179a43984
Yeshwantrao Tulshidas Naik <yeshwantraotulshidas.10838882@ltimindtree.com>
2025-08-18 14:25:53 +0530

    WF-3

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/')));
 app.use(cors())
 
-mongoose.connect('mongo+srv://supercluster.d83jj.mongodb.net/superData', {
-    user: superuser,
-    pass: SuperPassword,
+mongoose.connect(process.env.MONGO_URI, {
+    user: process.env.MONGO_USERNAME,
+    pass: process.env.MONGO_PASSWORD,
     useNewUrlParser: true,
     useUnifiedTopology: true
 }, function(err) {
